9fbe04244b026f3b7128e9b3b95a0dae03c261df
Matheus <matheusrodrigofilho@outlook.com>
2021-07-22 08:47:00 -0300

    search links

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,17 @@
 import * as fetch from 'node-fetch';
+import * as cherrio from 'cheerio';
 
-fetch('');
+const crawl = async ({ url }) => {
+  const response = await fetch(url);
+  const html = await response.text();
+
+  const $ = cherrio.load(html);
+  const links = $('a')
+    .map((i, link) => link.attribs.href)
+    .get();
+  console.log(links);
+
+  return true;
+};
+
+crawl({ url: 'http://localhost:10000/index.html' });
