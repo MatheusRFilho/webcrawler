fac356d53ab98cd1bec4d62fc8b519b8dca9f735
Matheus <matheusrodrigofilho@outlook.com>
2021-07-22 08:58:32 -0300

    feat: crawling all the pages in website

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
 import * as fetch from 'node-fetch';
 import * as cherrio from 'cheerio';
 
+const seenUrl = {};
 const crawl = async ({ url }) => {
+  if (seenUrl[url]) {
+    return;
+  }
+  console.log('crawling', url);
+  seenUrl[url] = true;
   const response = await fetch(url);
   const html = await response.text();
 
@@ -9,9 +15,23 @@ const crawl = async ({ url }) => {
   const links = $('a')
     .map((i, link) => link.attribs.href)
     .get();
-  console.log(links);
+  links.forEach((link) => {
+    crawl({
+      url: getUrl(link),
+    });
+  });
 
   return true;
+};
+
+const getUrl = (link) => {
+  if (link.includes('http')) {
+    return link;
+  } else if (link.startsWith('/')) {
+    return `http://localhost:10000${link}`;
+  } else {
+    return `http://localhost:10000/${link}`;
+  }
 };
 
 crawl({ url: 'http://localhost:10000/index.html' });
